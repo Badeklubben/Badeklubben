f316d8b7bff6139b51933edbe9c385ad2207ffe0
X104n <stianmunkejord@gmail.com>
2023-12-12 17:07:10 +0100

    Made my branch and added a lotto page for my lotto project

---
+++ src/app/stian/lotto/page.tsx
@@ -0,0 +1,7 @@
+export default function Lotto() {
+    return (
+        <div>
+        <h1>This is the Lotto page</h1>
+        </div>
+    );
+}
--- src/app/stian/page.tsx
+++ src/app/stian/page.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link";
+
 export default function Stian() {
     return (
         <div>
         <h1>This is the Stian page</h1>
+
+        <Link href="/stian/lotto">
+            <p>Lotto</p>
+        </Link>
+
         </div>
     );
 }
