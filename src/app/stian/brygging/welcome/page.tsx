7b5f4e0576bb9134b2f2bb9d08e2dbc8f2cf3421
X104n <stianmunkejord@gmail.com>
2023-12-14 15:22:52 +0100

    Some work from yeasterday but need to create a new branch for this project

--- src/app/stian/brygging/welcome/page.tsx
+++ src/app/stian/brygging/welcome/page.tsx
@@ -1,12 +1,22 @@
 'use client'
 import { useEffect, useState } from 'react';
 import app from '../lib/firebase.js';
-import { getAuth, onAuthStateChanged, User } from 'firebase/auth'; // Added User type import
+import { getAuth, onAuthStateChanged, signOut, User } from 'firebase/auth'; // Added User type import
 import { useRouter } from 'next/navigation';
 
 const Welcome = () => {
     const [user, setUser] = useState<null | User>(null); // Added null | User type for user state
+
     const router = useRouter();
+ 
+    const handleLogout = () => {               
+        signOut(getAuth(app)).then(() => {
+        // Sign-out successful.
+            router.push('/stian/brygging/');
+        }).catch((error) => {
+        // An error happened.
+        });
+    }
 
     useEffect(() => {
         const auth = getAuth(app);
@@ -21,7 +31,16 @@ const Welcome = () => {
         return () => unsubscribe();
     }, [router]);
 
-    return user ? <h1>Welcome</h1> : null;
+    return (
+        user ? 
+    <>
+        <button onClick={handleLogout}>Log out</button>
+        <h1>Welcome</h1>
+        <div>Test</div>
+    </> 
+    : 
+    <>Please log in</>
+    );
 };
 
 export default Welcome;
