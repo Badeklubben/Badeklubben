151faa60887beba94ba8dcf0387d153a2dc99527
Lars <Lars.bysheim@proton.me>
2024-08-20 16:31:13 +0200

    export const runtime = 'edge';

--- src/app/api/fetch_projects/route.ts
+++ src/app/api/fetch_projects/route.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import fs from 'fs';
 import path from 'path';
 
+export const runtime = 'edge';
 export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url);
   const directory = "src/app/projects/" + searchParams.get('id') || "";
