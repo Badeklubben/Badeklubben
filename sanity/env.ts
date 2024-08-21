429eedc045a08560fb837dfa38443c1b7db5ea61
Lars <Lars.bysheim@proton.me>
2024-08-21 16:33:46 +0200

    Prøver å endre på sanity variabler 2 : revenge of the bogaloo

--- sanity/env.ts
+++ sanity/env.ts
@@ -7,7 +7,7 @@ export const dataset = assertValue(
 )
 
 export const projectId = assertValue(
-  process.env.SANITY_STUDIO_PROJECT_ID,
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 )
 
