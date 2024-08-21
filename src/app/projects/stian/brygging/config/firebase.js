afae0dbbf9dd11efd3201bd18562674a48b0b2a7
Lars <Lars.bysheim@proton.me>
2024-08-21 16:13:43 +0200

    added missing imports

--- src/app/projects/stian/brygging/config/firebase.js
+++ src/app/projects/stian/brygging/config/firebase.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
