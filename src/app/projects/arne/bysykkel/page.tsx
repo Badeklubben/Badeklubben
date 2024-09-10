80f1a26f7abcdfbe18a9e5f4d5e9e083e3c3996c
ArneeMe <42463181+ArneeMe@users.noreply.github.com>
2024-09-10 14:51:38 +0200

    Bysykkel works now

--- src/app/projects/arne/bysykkel/page.tsx
+++ src/app/projects/arne/bysykkel/page.tsx
@@ -4,14 +4,14 @@ import Link from "next/link";
 import React from "react";
 import Box from "@mui/material/Box"
 import DefaultTypography from "@/app/shared/components/DefaultTypography";
+import BikeInfo from "@/app/projects/arne/bysykkel/BikeInfo";
 
 export default function CityBike() {
     return (
         <Box>
             <DefaultDrawer></DefaultDrawer>
-            <DefaultTypography fontWeight={700} colorText={"#000"}>Velkommen til Arne sin side!</DefaultTypography>
-
-
+            <DefaultTypography fontWeight={700} colorText={"#000"}>Sjekk om bysykkel er tilgjengelig</DefaultTypography>
+            <BikeInfo />
         </Box>
     );
 }
