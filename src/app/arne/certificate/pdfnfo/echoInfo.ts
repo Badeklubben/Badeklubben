6b39b865152390c7e2abf98e1117cc18cbef0e9f
ArneeMe <42463181+ArneeMe@users.noreply.github.com>
2025-01-13 19:31:20 +0100

    Extracted and reformated

---
+++ src/app/arne/certificate/pdfnfo/echoInfo.ts
@@ -0,0 +1,13 @@
+export const generic_echo: string =
+    "echo er linjeforeningen for informatikk ved Universitetet i Bergen, og består av over 100 frivillige studenter. " +
+    "Foreningens hovedoppgave er å bidra positivt til det sosiale og akademiske miljøet for 1000 informatikkstudenter. " +
+    "echo sitt mål er å fungere som et bindeledd mellom studentene og universitetet, og samtidig være en viktig arena for informatikkstudentenes sosiale liv. "
+
+export const undergrupper: Record<string, string> = {
+    'Bedkom': 'Bedriftskomitéen ved echo tilbyr hjelp med planlegging, markedsføring og organisering av ulike arrangement for bedrifter. Dette kan for eksempel være bedriftspresentasjoner eller workshops. Vi tilbyr rådgivning til bedrifter om hvordan de kan kommunisere effektivt og målrettet med våre studenter, og være en positiv faglig bidragsyter ved Institutt for Informatikk.',
+    'ESC': 'Undergruppen skal fungere som et bindeledd mellom flere idrettslag under echo, slik at det skal bli lettere å forme ulike idrettsgrupper på informatikk. De skal også være med å arrangere treninger, fikse hall-leie og holde kurs.',
+    'Gnist': 'Gnist er en undergruppe bestående av studenter underlagt echo, med fokus på rekruttering og fullføring. Vi jobber tett sammen med instituttet og fakultetet om saker som angår frafallsproblematikk og rekruttering av nye studenter. Med støtte fra instituttet kan Gnist gjennomføre tiltak vi mener vil være positivt for studentene på instituttet, både faglig og sosialt!',
+    'Hyggkom': 'Hyggkom er en undergruppe under echo med hovedfokus å skape trivsel på lesesalen. De arrangerer alt fra julekalender til påskegggjakt i løpet av semesteret. Målet er i hovedsak å gi informatikkstudenter et lite avbrekk i en tung og hektisk hverdag!',
+    'Tilde': 'Tilde er en arbeidsgruppe bestående av studenter underlagt echo, med fokus på å arrangere og gjennomføre sosiale arrangementer. Dette kan være turneringer, quiz, fester, lan og mye mer. Målet med Tilde er å styrke det sosiale miljøet på institutt for informatikk, og å lage arrangementer for alle våre studenter.',
+    'Webkom': 'Webkom er en undergruppe som drifter og videreutvikler echo sine webløsninger, blant annet denne nettsiden.  Ved hjelp av prosjektarbeid og en arbeidsmåte som gjenspeiler arbeidslivet, gir vi våre medlemmer en smakebit på hvordan det er å være utvikler.  Noe av det du kan være med å utvikle som medlem i Webkom, er blant annet publiseringssytemet til nettsiden, påmeldingsløsninger til bedriftspresentasjoner og arrangementer, eller design av ulike interne webapplikasjoner.',
+}
