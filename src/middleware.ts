2d3407acc3127553f92da45bd4854543cf1f964a
X104n <github.freeware171@passmail.net>
2025-05-04 15:15:27 +0200

    Experimental edge

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export const runtime = "edge";
+export const runtime = "experimental-edge";
 
 export function middleware(request: NextRequest) {
   // Add a new header x-current-path which passes the path to downstream components
