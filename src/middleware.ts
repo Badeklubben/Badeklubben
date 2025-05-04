10d4677cee45b1436125de5bede072c4b28a65df
X104n <github.freeware171@passmail.net>
2025-05-04 15:05:37 +0200

    Runtim edge middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
+export const runtime = "edge";
+
 export function middleware(request: NextRequest) {
   // Add a new header x-current-path which passes the path to downstream components
   const headers = new Headers(request.headers);
