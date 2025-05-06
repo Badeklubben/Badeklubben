a1a52298ca1f1076f4e15135452c466855c42792
Gard <github.blunderer148@passmail.net>
2025-05-07 01:43:26 +0200

    Runtime wuhooo

--- apps/www/middleware.ts
+++ apps/www/middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export const runtime = "experimental-edge";
+export const runtime = "edge";
 
 export function middleware(request: NextRequest) {
   // Add a new header x-current-path which passes the path to downstream components
