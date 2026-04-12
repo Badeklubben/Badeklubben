c6559dbaa8a75ae5099c19039127505422031f16
Arne Natskår <42463181+ArneeMe@users.noreply.github.com>
2026-04-12 22:21:12 +0200

    Update apartments.ts
    oppdater path navn

--- src/app/projects/arne/voting/apartments.ts
+++ src/app/projects/arne/voting/apartments.ts
@@ -31,7 +31,7 @@ export const apartments: Apartment[] = [
         link: "https://www.airbnb.com/rooms/961685669302177270",
         price: 35200,
         beds: 4,
-        img: "/img/ap1.jpg",
+        img: "/img/ap1.jpeg",
         info: {
             pros: [
                 "Stille nabolag, pool-venner sent på kvelden",
@@ -56,7 +56,7 @@ export const apartments: Apartment[] = [
         link: "https://www.airbnb.com/rooms/1036960558401027424",
         price: 45000,
         beds: 3,
-        img: "/img/ap2.jpg",
+        img: "/img/ap2.jpeg",
         info: {
             pros: [
                 "3 ekte king size-senger — alle i egne rom",
@@ -82,7 +82,7 @@ export const apartments: Apartment[] = [
         link: "https://www.airbnb.com/rooms/50666941",
         price: 39000,
         beds: 4,
-        img: "/img/ap3.jpg",
+        img: "/img/ap3.jpeg",
         info: {
             pros: [
                 "Beachfront — direkte strandtilgang",
